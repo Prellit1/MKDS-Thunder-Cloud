4692aae3c8286751e7de848de71a7c2ae531c7d3
Prellit1 <ethan.100unpro@gmail.com>
2025-08-17 19:38:42 +0200

    Initialise repository for the Thunder Cloud !!!!!!!!!!!!!

---
+++ src/thunder_cloud.c
@@ -0,0 +1,337 @@
+//FILE CREATED BY PERLITE ON 2025/08/12
+#include "thunder_cloud.h"
+
+#define ITEM_TERESA 11
+
+//SOUND POTENTIAL : 73, 94, 233 (For the TC in await mode)
+
+void init_thunder_inst(it_thunder_inst_t*);
+void use_thunder_inst(void*, it_thunder_inst_t*);
+void update_thunder_inst(it_thunder_inst_t*);
+void await_thunder_inst(it_thunder_inst_t*);
+void init_thunder_model(it_thunder_inst_t*);
+void shocking_thunder_inst(it_thunder_inst_t*);
+void shock_thunder_inst(it_thunder_inst_t*);
+void killing_thunder_inst(it_thunder_inst_t*);
+void emit_thunder_poof_particle(it_thunder_inst_t*);
+void callback_PcEm_stay_at_inst(void*);
+void render_thunder_inst(it_thunder_inst_t*, VecFx32*, void*);
+
+it_itemconfig_t cfg_thunder_cloud = {1,1,ITEM_TERESA, 1, 0,1, (void*)0x02102668};
+
+it_item_def_t def_thunder_cloud = {
+    sizeof(it_thunder_inst_t),
+    1,
+    1,
+    -1,
+    -1,
+    NULL,
+    init_thunder_inst,
+    NULL,
+    NULL,
+    use_thunder_inst,
+    update_thunder_inst,
+    render_thunder_inst,
+    (void*) 0x01FFD778,
+    NULL,
+    NULL,
+    NULL,
+    0,
+    0,
+    (void*)0x021004C4,
+    0,
+    init_thunder_model,
+    0,
+    0,
+    0x4B0000,
+    2,
+    0,
+    0,
+    0,
+    0x1000,
+    0x1800,
+    0x333,
+    0x11f,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0
+}; //Most attributes copied from the base ghost definition structure
+
+char it_teresa_nsbmd[16] ="it_teresa.nsbmd";
+char it_teresa_nsbtp[16] ="it_teresa.nsbtp";
+asm(R"(
+    .thumb
+    ncp_tcall(0x020F4D00)
+        STR     R0, [R4]
+        LDR     R5, =cfg_thunder_cloud
+
+       @ LDR     R0, =0x1C0
+        @ADDS    R4, R3, R0
+
+        bx lr
+
+
+    ncp_tcall(0x020F1BEC)
+        LDR     R5, =def_thunder_cloud
+        LDR     R0, =0x738
+
+       @ ADDS    R4, R2, R0
+        @MOVS    R3, #0x15
+
+        bx lr
+    
+    
+    ncp_tcall(0x020ED39C)
+        ldr r0, =it_teresa_nsbmd
+        ldr r1, =it_teresa_nsbtp
+        bx lr
+)");
+
+void* state_table[] = {
+        NULL,                       await_thunder_inst, 
+        shock_thunder_inst,        shocking_thunder_inst, 
+        emit_thunder_poof_particle, killing_thunder_inst
+        };
+
+void init_thunder_inst(it_thunder_inst_t* inst){
+    /*
+    ENTRY   : TC inst
+    ROLE    : Initializes the TC's struct and state machine
+    VERSION : 2025AUG_04
+    AUTHOR  : Perlite
+    */
+    
+    sm_init(&inst->stateMachine, state_table, (int)(sizeof(state_table) / sizeof(void*)) >> 1, inst);
+    inst->stateMachine.counter = 0;
+    inst->lastHitCounter = 0;
+    inst->particleCounter = 0;
+    inst->thunderBeat = 25;
+    inst->frameTime = 0;
+    inst->texID = 0;
+};
+
+void update_thunder_inst(it_thunder_inst_t* inst){
+    /*
+    ENTRY   : TC inst
+    ROLE    : Sets the TC's X and Z position and prepares the Y position set by the current state of the TC. Also counts the age of the TC and executes the current TC state's function
+    VERSION : 2025AUG_02
+    AUTHOR  : Perlite
+    */
+
+    void* driver = driver_getById((int)inst->targetDriverId);
+    VecFx32* drivPos = (VecFx32*)(driver + 0x80);
+    VecFx32* drivDir = (VecFx32*)(driver + 0x50);
+    VecFx32 target = {0};
+    VEC_MultAdd(FX32_CONST(8), drivDir, drivPos, &target);
+
+    inst->inst.position.x = inst->inst.position.x + FX_MUL(target.x - inst->inst.position.x, FX32_CONST(0.4));
+    inst->preYPosition = inst->preYPosition + FX_MUL(target.y + FX32_CONST(32) - inst->preYPosition, FX32_CONST(0.4));
+    inst->inst.position.z = inst->inst.position.z + FX_MUL(target.z - inst->inst.position.z, FX32_CONST(0.4));
+
+    inst->frameTime += 1;
+
+    sm_execute(&inst->stateMachine);
+};
+
+void await_thunder_inst(it_thunder_inst_t* inst){
+    /*
+    ENTRY   : TC inst
+    ROLE    : Handles the base state of the TC by emitting particles, changing its texture and making it move slightly verically and changing slightly its scale depending on the state's counters and the TC's age. Also makes the TC change state after some time
+    VERSION : 2025AUG_02
+    AUTHOR  : Perlite
+    */
+
+    if (!inst->texTime){
+        inst->texID = 0;
+    } else {
+        inst->texID = 1;
+        inst->texTime -=1;
+    }
+
+    void* driver = driver_getById((int)inst->targetDriverId);
+
+    inst->inst.position.y = inst->preYPosition + 2 * FX_SinIdx((u16)((inst->frameTime + 15) * 512));
+    inst->inst.scale.x = FX32_CONST(1.7) + (FX_CosIdx((u16)(inst->frameTime * 1024)) >> 4);
+    inst->inst.scale.y = FX32_CONST(1.7) + (FX_SinIdx((u16)(inst->frameTime * 1024)) >> 4);
+
+    u16 hitMask = *(u16*)(driver + 0x2B2);
+    for (int id = 0; id < driver_sDriverCount; id++){
+        if (id != inst->targetDriverId && ((hitMask >> id) & 1) && inst->stateMachine.counter - inst->lastHitCounter > 30){
+            inst->targetDriverId = id;
+            inst->lastHitCounter = inst->stateMachine.counter;
+        }
+    }
+
+    if (inst->stateMachine.counter > 60*12) {
+        inst->stateMachine.gotoNextState = 1;
+        inst->stateMachine.nextState = 1;
+    }
+
+    if (inst->stateMachine.counter - inst->particleCounter > 80){
+
+        if (inst->particleEmitter){
+            ptcm_killEmitterDirectSafe(&inst->particleEmitter);
+        }
+
+        inst->particleEmitter = ptcm_createEmitter(94, &inst->inst.position);
+        *(void**)(inst->particleEmitter + 0x90) = callback_PcEm_stay_at_inst;
+        *(it_thunder_inst_t**)(inst->particleEmitter + 0x98) = inst;
+        inst->particleCounter = inst->stateMachine.counter;
+    }
+
+    if (inst->stateMachine.counter == inst->particleCounter && inst->particleEmitter){
+        sfx_emit_item_inst(&inst->inst.sfx_emitter, 233, 127, 0);
+        inst->texTime = 10;
+    }
+    if (inst->thunderBeat == 40 && inst->stateMachine.counter - inst->particleCounter == 20){
+        sfx_emit_item_inst(&inst->inst.sfx_emitter, 233, 127, 0);
+        inst->texTime = 10;
+    }
+
+    if (inst->stateMachine.counter - inst->particleCounter > inst->thunderBeat  && inst->particleEmitter){
+        ptcm_killEmitterDirectSafe(&inst->particleEmitter);
+        inst->particleEmitter = NULL;
+    }
+    
+    if (inst->stateMachine.counter == 60*7) inst->thunderBeat = 40;
+}
+
+void use_thunder_inst(void* item_drag, it_thunder_inst_t* inst){
+    /*
+    ENTRY   : item_drag struct for the TC, TC inst
+    ROLE    : Initializes the TC's sound emitter and position and state as the function is called when the TC is used
+    VERSION : 2025AUG_02
+    AUTHOR  : Perlite
+    */
+    it_init_item_inst_sound_emitter(inst);
+
+    inst->targetDriverId = *(int*)(item_drag + 0x28);
+    void* driver = driver_getById(inst->targetDriverId);
+    VecFx32* drivPos = (VecFx32*)(driver + 0x80);
+    inst->inst.position.x = drivPos->x;
+    inst->preYPosition = drivPos->y;
+    inst->inst.position.y = drivPos->y;
+    inst->inst.position.z = drivPos->z;
+
+    sm_gotoState(&inst->stateMachine, 0);
+}
+
+void init_thunder_model(it_thunder_inst_t* inst){
+    /*
+    ENTRY   : TC inst
+    ROLE    : Initializes the TC's model with a function already present in the game
+    VERSION : 2025AUG_02
+    AUTHOR  : Perlite
+    */
+
+    it_item_model_coli_init(inst);
+}
+
+void shock_thunder_inst(it_thunder_inst_t* inst){
+    /*
+    ENTRY   : TC inst
+    ROLE    : Shocks the driver and changes the TC's texture
+    VERSION : 2025AUG_02
+    AUTHOR  : Perlite
+    */
+
+    void* driver = driver_getById(inst->targetDriverId);
+    race_startDarkening();
+    sub_2082E4C(*(u16*)(driver + 0x74)); //Needs update and studying, spawns lightnings for everyone else
+    driver_startThunderShrinkEffect(driver);
+
+    if (inst->particleEmitter){
+        ptcm_killEmitterDirectSafe(&inst->particleEmitter);
+        inst->particleEmitter = NULL;
+    }
+    inst->texID = 1;
+}
+
+void shocking_thunder_inst(it_thunder_inst_t* inst){
+    /*
+    ENTRY   : TC inst
+    ROLE    : Handles the TC in its shocking state by making it vibrate faster and changes the state after some time
+    VERSION : 2025AUG_01
+    AUTHOR  : Perlite
+    */
+
+    inst->inst.position.y = inst->preYPosition;
+    inst->inst.scale.x = FX32_CONST(1.7) + (FX_CosIdx((u16)((inst->frameTime) * 4096)) >> 4);
+    inst->inst.scale.y = FX32_CONST(1.7) + (FX_SinIdx((u16)((inst->frameTime) * 4096)) >> 4);
+
+    if (inst->stateMachine.counter > 75){
+        inst->stateMachine.gotoNextState = 1;
+        inst->stateMachine.nextState = 2;
+    }
+}
+
+void callback_PcEm_stay_at_inst(void* emitter){
+    /*
+    ENTRY   : Particle emitter
+    ROLE    : Makes the PcEm stay at the TC instance
+    VERSION : 2025AUG_01
+    AUTHOR  : Perlite
+    */
+
+    it_thunder_inst_t* userWork = *(it_thunder_inst_t**)(emitter + 0x98);
+    VecFx32* emit_pos =(VecFx32*)(emitter + 0x28);
+    emit_pos->x = userWork->inst.position.x >> 4;
+    emit_pos->y = userWork->inst.position.y >> 4;
+    emit_pos->z = userWork->inst.position.z >> 4;
+}
+
+void emit_thunder_poof_particle(it_thunder_inst_t* inst){
+    /*
+    ENTRY   : TC inst
+    ROLE    : Emits a cloud particle to allow a non offputting disappearance of the TC instance
+    VERSION : 2025AUG_01
+    AUTHOR  : Perlite
+    */
+
+    inst->particleEmitter = ptcm_createEmitter(97, &inst->inst.position); //maybe 89 ? 97 ? 70 ?
+    *(void**)(inst->particleEmitter + 0x90) = callback_PcEm_stay_at_inst;
+    *(it_thunder_inst_t**)(inst->particleEmitter + 0x98) = inst;
+}
+
+void killing_thunder_inst(it_thunder_inst_t* inst){
+    /*
+    ENTRY   : TC inst
+    ROLE    : Keeps the instance alive until the particle is thought to have finished its animation, then kills both the particle emitter and the instance
+    VERSION : 2025AUG_01
+    AUTHOR  : Perlite
+    */
+
+    void* driver = driver_getById((int)inst->targetDriverId);
+    VecFx32* drivPos = (VecFx32*)(driver + 0x80);
+
+    inst->inst.position.y = inst->preYPosition;
+    
+    if (inst->stateMachine.counter > 60) {
+        if (inst->particleEmitter){
+            ptcm_killEmitterDirectSafe(&inst->particleEmitter);
+            inst->particleEmitter = NULL;
+        }
+        it_itemsetDestroyItem((it_sItemsets + 0x44 * inst->inst.type), inst);
+    }
+}
+
+void render_thunder_inst(it_thunder_inst_t* inst, VecFx32* pos, void* model){
+    /*
+    ENTRY   : TC inst, position vector, model
+    ROLE    : Allows the TC instance to be rendered unless its being killed, handles texture changes and uses an already existing function to render the instance
+    VERSION : 2025AUG_01
+    AUTHOR  : Perlite
+    */
+
+    if (inst->stateMachine.curState != 2){
+        *(s16*)(model + 0x18) = inst->texID;
+        it_render_inst(inst, model, pos);
+    }
+}
---
+++ src/thunder_cloud.h
@@ -0,0 +1,155 @@
+//FILE CREATED BY PERLITE ON 2025/08/12
+#include "mkds.h"
+
+
+typedef struct
+{
+    u32 instanceSize; // Use sizeof 
+    u32 limit; //max amount of item instances allows
+    u32 field8; //unsure on the purpose of this Possible values are:
+
+    int instanceCount; //-1 on every item probably set somewhere else
+    int field10; //-1 on every item probably set somewhere else
+    void* loadFunc; //LoadFunction used in kouraW to load the model also used in most of the single use items
+    void* initInstanceFunc; //Similar to the init functions on mapobjs
+    void* field1C;
+    void* field20;
+    void* deployFunc; //used by gesso to play the deploy sound, called when the player stops dragging the ittem
+    void* updateFunc;
+    void* renderFunc;
+    void* visibilityFlagCalcFunc; //Used to calculate if an object should be visible
+    void* field34;
+    void* field38;
+    void* destroyInstFunc;
+    int field40;
+    int field44;
+    void* field48;
+    int field4C;
+    void* field50;
+    u32 gap54;
+    void* field58; //related to sound emission?
+    int field5C;
+    int field60;
+    int colSphereRadius;
+    int sphereRadius1;
+    int sphereRadius2;
+    int field70;
+    int scale;
+    int field78;
+    int field7C;
+    int field80;
+    int field84;
+    int field88;
+    u32 gap8C;
+    int field90;
+    int field94;
+    int field98;
+    int field9C;
+    int fieldA0;
+    int fieldA4;
+} it_item_def_t; //Base Game item definition structure. Copied from Rocoloco321's research document 
+
+
+typedef struct
+{
+    int enabled; //Will not appear on regular gameplay if false???
+    int wifiEnabled;  //Will not appear on Wifi if false
+    int type; //Object type, spawns the object id that is given
+    u32 count; // Amount of times it can be used???
+    u32 field10; //Unsure on what this do. 0 on every item minus the golden mushroom
+    u32 field14;// possible values are 0-4
+    void* activateFunc; //Activation function, called when the item is used, seems to be used only on items that have special behavior, 0 when it’s unused
+} it_itemconfig_t; //Base Game item config structure. Copied from Rocoloco321's research document 
+
+typedef struct
+{
+    char sfx_emitter[0x44];
+    u32 type;
+    u32 field48;
+    u16 field4C;
+    u16 field4E;
+    VecFx32 position;
+    VecFx32 velocity;
+    VecFx32 scale;
+    u32 flags;
+    u16 field78;
+    u16 field7A;
+    char light[0x14];
+    void* lightPTR;
+    char mtx[0x3C];
+    u32 fieldD0;
+    u32 visibilityFlags;
+    u16 alpha;
+    u16 colEntryId;
+    u32 fieldDC;
+    u32 sphereSize;
+    VecFx32 fieldE4;
+    VecFx32 fieldF0;
+    u32 fieldFC;
+    u32 field100;
+    u32 field104;
+    VecFx32 field108;
+    u16 field114;
+    u16 field116;
+    u16 field118;
+    u16 field11A;
+    u32 field11C;
+    void* update_func_IDK;
+    u32 field124;
+    u16 field128;
+    u16 field12B;
+} it_item_inst_t; //Base Game parent item instance structure
+
+typedef struct{
+    void** pStates;
+    u32 counter;
+    void* pUserData;
+    u16 nrStates;
+    u16 curState;
+    u16 nextState;
+    u16 gotoNextState;
+} state_machine_t; //Base Game state machine structure
+
+typedef struct{
+    it_item_inst_t inst;
+    u16 targetDriverId;
+    u16 thunderBeat;
+    u16 lastHitCounter;
+    u16 particleCounter;
+    u32 frameTime;
+    u16 texTime;
+    u16 texID;
+    state_machine_t stateMachine;
+    void* particleEmitter;
+    int preYPosition;
+} it_thunder_inst_t; //Children structure of the item instance structure
+
+//External Variables
+extern void* it_sItemsets;
+extern s16 driver_sDriverCount;
+extern void* mobj_calcVisibilityFlags2d;
+extern void* r2d_sStatus; 
+
+//External Functions
+extern void sub_20F8E78(it_item_inst_t*);
+extern void sm_execute(state_machine_t*);
+extern void sm_gotoState(state_machine_t*, int);
+extern void it_itemsetDestroyItem(void*, it_item_inst_t*);
+extern void sm_init(state_machine_t*, void*, int, it_thunder_inst_t*);
+extern void sub_2082E4C(u16);
+extern void race_startDarkening();
+extern void driver_startThunderShrinkEffect(void*);
+extern void ptcm_killEmitterDirectSafe(void*);
+extern void* ptcm_createEmitter(int, VecFx32*);
+extern void sfx_21090C4(void*,int,char,short);
+extern void sub_20F9114(it_item_inst_t*);
+extern void VEC_MultAdd(int, VecFx32*, VecFx32*, VecFx32*);
+extern void sub_20ED5C0(it_item_inst_t*, void*, VecFx32*);
+
+//Renaming of certain External Functions
+#define it_item_model_coli_init sub_20F8E78
+#define sfx_emit_item_inst sfx_21090C4
+#define it_init_item_inst_sound_emitter sub_20F9114
+#define it_render_inst sub_20ED5C0
+
+
